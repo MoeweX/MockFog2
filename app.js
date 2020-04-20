5f6f439395ab21aa518f8670f8591f04e809c2c6
Jonathan Hasenburg <Jonathan@Hasenburg.de>
2020-04-20 14:01:43 +0000

    Add common.js

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 const fs = require("fs")
 
 const conf = require("./lib/config.js")
-const Bootstrap = require("./lib/phases/02_bootstrap.js")
+const bootstrap = require("./lib/phases/02_bootstrap.js")
 
 console.log("MockFog2 is setup.")
 
-const bootstrap = new Bootstrap()
 const bootstrapLog = conf.runLogDir + "bootstrap-playlog.log"
 
 fs.unlink(bootstrapLog, function(err) {
     // err can be ignored, if it did not exist -> fine
 
-    bootstrap.on("log", function(data) {
-        console.log(data)
-    })
-
     bootstrap.on("playlog", function(data) {
         fs.appendFile(bootstrapLog, data, { "flag": "a+" }, (err) => { if (err) throw err})
+        bootstrap.stop()
     })
 
     bootstrap.on("done", function(data) {
