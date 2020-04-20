b128eccc097327a5a8ba2c939f0f3814203d0b44
Jonathan Hasenburg <Jonathan@Hasenburg.de>
2020-04-20 14:45:30 +0000

    Add destroy js code

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
 const fs = require("fs")
 
 const conf = require("./lib/config.js")
-const bootstrap = require("./lib/phases/02_bootstrap.js")
+
+// so that everything is setup
+require("./lib/phases/02_bootstrap.js")
+require("./lib/phases/07_destroy.js")
 
 console.log("MockFog2 is setup.")
 
-const bootstrapLog = conf.runLogDir + "bootstrap-playlog.log"
+function doBootstrap() {
+    const bootstrap = require("./lib/phases/02_bootstrap.js")
+    const bootstrapLog = conf.runLogDir + "bootstrap-playlog.log"
+    fs.unlink(bootstrapLog, function(err) {
+        // err can be ignored, if it did not exist -> fine
 
-fs.unlink(bootstrapLog, function(err) {
-    // err can be ignored, if it did not exist -> fine
+        bootstrap.on("playlog", function(data) {
+            fs.appendFile(bootstrapLog, data, { "flag": "a+" }, (err) => { if (err) throw err})
+        })
 
-    bootstrap.on("playlog", function(data) {
-        fs.appendFile(bootstrapLog, data, { "flag": "a+" }, (err) => { if (err) throw err})
-        bootstrap.stop()
+        bootstrap.on("done", function(data) {
+            console.log("Bootstrap done, exit code is: " + data)
+        })
+
+        bootstrap.start()
+
     })
+}
 
-    bootstrap.on("done", function(data) {
-        console.log("Bootstrapping done, exit code is: " + data)
+function doDestroy() {
+    const destroy = require("./lib/phases/07_destroy.js")
+    const destroyLog = conf.runLogDir + "destroy-playlog.log"
+    fs.unlink(destroyLog, function(err) {
+        // err can be ignored, if it did not exist -> fine
+
+        destroy.on("playlog", function(data) {
+            fs.appendFile(destroyLog, data, { "flag": "a+" }, (err) => { if (err) throw err})
+        })
+
+        destroy.on("done", function(data) {
+            console.log("Destroy done, exit code is: " + data)
+        })
+
+        destroy.start()
+
     })
+}
 
-    bootstrap.start()
-
-})
+doDestroy()
