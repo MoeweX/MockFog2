1220ac81250c61be460d54b56a140f775068cfd4
Jonathan Hasenburg <Jonathan@Hasenburg.de>
2020-05-06 14:56:33 +0000

    Agent deployment mostly completed

--- agent/app.js
+++ agent/app.js
@@ -3,7 +3,7 @@ const networkService = require("./services/networkService.js")
 const networkController = require("./controllers/networkController.js")
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.AGENT_PORT || 3000
 
 // setup viewengine
 app.set("view engine", "ejs");
@@ -16,4 +16,5 @@ app.get("/", function(req, res) {
 // setup controllers
 networkController(app)
 
+console.log("Agent started, listening on port " + port)
 app.listen(port)
