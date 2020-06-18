962e3729d50f853fa4bdac0b975ce14682f6ba7e
Jonathan Hasenburg <Jonathan@Hasenburg.de>
2020-06-18 14:56:32 +0000

    Add internal_ip_container function (untested)

--- node-manager/lib/data/magic-functions.js
+++ node-manager/lib/data/magic-functions.js
@@ -1,6 +1,7 @@
 const logger = require("../services/logService.js")("magic")
 
 const machineMeta = require("./machine-meta.js")
+const deployment = require("./deployment.js")
 
 /**
  * Checks whether the given value string is a magic function. 
@@ -23,6 +24,26 @@ function evaluateMagicFunction(valueString) {
 function internal_ip(machine_name) {
     // get latest machine metadata
     const mm = machineMeta()
+
+    return mm.getInternalIP(machine_name)
+}
+
+function internal_ip_container(container_name) {
+    // get latest machine metadata
+    const mm = machineMeta()
+    const dm = deployment()
+
+    const machineNames = dm.getMachineNames(container_name)
+    let machine_name = ""
+    if (machineNames.length === 0 ) {
+        logger.error(`Container ${container_name} is not deployed on any container`)
+    } else if (machineNames.length === 1) {
+        logger.debug(`Container ${container_name} is deployed on machine ${machineNames[0]}`)
+        machine_name = machineNames[0]
+    } else {
+        logger.warn(`Container ${container_name} is deployed on multiple machines ${machineNames}, returning the internal ip of the first one`)
+        machine_name = machineNames[0]
+    }
 
     return mm.getInternalIP(machine_name)
 }
