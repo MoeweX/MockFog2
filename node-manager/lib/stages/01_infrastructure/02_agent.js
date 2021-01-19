cb8fa8c23c922a38c991ea03f2cf9284e3b2b6d1
Jonathan Hasenburg <Jonathan@Hasenburg.de>
2021-01-19 10:57:02 +0000

    Add netplan file generation

--- node-manager/lib/stages/01_infrastructure/02_agent.js
+++ node-manager/lib/stages/01_infrastructure/02_agent.js
@@ -2,6 +2,7 @@ const fs = require("fs")
 const fsp = fs.promises
 
 const infrastructure = require("../../data/infrastructure.js")
+const machineMetaF = require("../../data/machine-meta.js")
 
 const common = require("../common.js")
 const Phase = require("../phase.js")
@@ -20,12 +21,25 @@ class Child extends Phase {
 
     async parseInput() {
         this.infra = infrastructure()
+        this.machineMeta = machineMetaF()
     }
 
     async runPrePlaybookTasks() {
         // write var file
         await fsp.writeFile(this.varPath, this.infra.awsYML)
         this.logger.info("Agent playbook vars written to " + this.varPath)
+
+        // write netplan file
+        for (const machine of this.infra.infra.machines) {
+            const machine_name = machine.machine_name
+            const netplan_string = this.machineMeta.getNetplanString(machine_name)
+            const machine_dir = conf.runMachinesDir + machine_name
+            conf.checkFolderExists(machine_dir)
+            const netplan_path = machine_dir + "/netplan.yml"
+
+            await fsp.writeFile(netplan_path, netplan_string)
+            this.logger.info("Wrote netplan file for " + machine_name + " to " + netplan_path)
+        }
 
         // create playbook object
         this.playbook = new common.Playbook(this.playbookPath, this.varPath, ["-i", `${conf.runDir}hosts`, `--key-file=${conf.runDir}${this.infra.infra.aws.ssh_key_name}.pem`])
